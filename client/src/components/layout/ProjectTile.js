6f8a2cf79a31511bde7f74c3feceb9f4e817a706
JCWoodworker <jfc3303@gmail.com>
2022-02-07 21:09:12 -0500

    fixed $ issue

--- client/src/components/layout/ProjectTile.js
+++ client/src/components/layout/ProjectTile.js
@@ -5,9 +5,9 @@ const ProjectTile = props => {
   return (
     <>
       <div className="project-tile">
-        <p><strong>Project Name: </strong>${props.project.name}</p>
-        <p><strong>Description: </strong>${props.project.description}</p>
-        <p><strong>Primary Language: </strong>${props.project.language}</p>
+        <p><strong>Project Name: </strong>{props.project.name}</p>
+        <p><strong>Description: </strong>{props.project.description}</p>
+        <p><strong>Primary Language: </strong>{props.project.language}</p>
       </div>
       <div className="tile-underline"></div>
     </>
